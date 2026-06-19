d8dfe566603c90aa4ccb5179fcc615ab7d03b493
Matthew Vaishnav <211710632+matthewvaishnav@users.noreply.github.com>
2026-06-19 15:42:56 -0400

    Place conclusion on standalone single-column page

--- paper/build.ps1
+++ paper/build.ps1
@@ -1,8 +1,8 @@
-$ErrorActionPreference = "Stop"
-
+﻿$ErrorActionPreference = "Stop"
 Set-Location $PSScriptRoot
 
-pdflatex -interaction=nonstopmode main.tex
-pdflatex -interaction=nonstopmode main.tex
+pdflatex -interaction=nonstopmode -halt-on-error main.tex
+pdflatex -interaction=nonstopmode -halt-on-error main.tex
 
-Write-Host "Built paper/main.pdf"
+Copy-Item main.pdf pathoalign-external-caninescc.pdf -Force
+Write-Host "Built paper\pathoalign-external-caninescc.pdf"
